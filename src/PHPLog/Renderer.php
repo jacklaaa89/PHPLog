2f63a158f87f3abda417b8d806f2c26c67750d80
Jack Timblin <jacktimblin@gmail.com>
2015-06-15 13:52:33 +0100

    handled object type rendering better in the default renderer.

--- src/PHPLog/Renderer.php
+++ src/PHPLog/Renderer.php
@@ -138,8 +138,14 @@ class Renderer {
 			return $this->defaultRenderer->render($object);
 		}
 
-		//if there was no default, attempt to cast it to a string.
+		//attempt to cast the object to a string.
 		$object = ($object !== null) ? $object : '';
+		//check to see if this variable is an array or object.
+		if(is_array($object) || is_object($object)) {
+			ob_start();
+			var_dump($object);
+			$object = ob_get_clean();
+		}
 		$object = (!is_string($object)) ? (string) $object : $object;
 
 		return $object;
--- src/PHPLog/Renderer/DefaultRenderer.php
+++ src/PHPLog/Renderer/DefaultRenderer.php
@@ -20,6 +20,12 @@ class DefaultRenderer implements RendererInterface {
 
 		//attempt to cast the object to a string.
 		$object = ($object !== null) ? $object : '';
+		//check to see if this variable is an array or object.
+		if(is_array($object) || is_object($object)) {
+			ob_start();
+			var_dump($object);
+			$object = ob_get_clean();
+		}
 		$object = (!is_string($object)) ? (string) $object : $object;
 
 		return $object;
